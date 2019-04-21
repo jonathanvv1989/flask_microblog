20a9e101911fe4b19dcdbf6df3530b5aebae2682
Jonathan <jonathan_vincentviry@yahoo.fr>
2019-04-21 22:54:20 +0200

    Update quotes on homepage

--- app/routes.py
+++ app/routes.py
@@ -7,11 +7,13 @@ def index():
     user = {"username": "Jonathan"}
     posts = [
         {"author": {"username": "Kerry"},
-         "body": "Women are powerful"},
+         "body": "Gender equality is the way forward"},
         {"author": {"username": "Rennor"},
          "body": "Norway is the most powerful country"},
         {"author": {"username": "Yanglang"},
-         "body": "Love is everything"}
+         "body": "Love is everything"},
+        {"author": {"username": "Jonathan"},
+         "body": "Knowledge is your inspiration"}
     ]
 
     return render_template("index.html", title="Home", user=user, posts=posts)
