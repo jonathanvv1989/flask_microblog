abeb93dafed71f68f0ae84ef1b3e8cf06c9173e8
Jonathan <jonathan_vincentviry@yahoo.fr>
2019-04-22 11:08:11 +0200

    (Form 1.2) Add login form

--- app/routes.py
+++ app/routes.py
@@ -1,19 +1,26 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
-@app.route("/")
-@app.route("/index")
+
+@app.route('/')
+@app.route('/index')
 def index():
-    user = {"username": "Jonathan"}
+    user = {'username': 'Jonathan'}
     posts = [
-        {"author": {"username": "Kerry"},
-         "body": "Gender equality is the way forward"},
-        {"author": {"username": "Rennor"},
-         "body": "Norway is the most powerful country"},
-        {"author": {"username": "Yanglang"},
-         "body": "Love is everything"},
-        {"author": {"username": "Jonathan"},
-         "body": "Knowledge is your inspiration"}
+        {'author': {'username': 'Kerry'},
+         'body': 'Gender equality is the way forward'},
+        {'author': {'username': 'Rennor'},
+         'body': 'Norway is the most powerful country'},
+        {'author': {'username': 'Yanglang'},
+         'body': 'Love is everything'},
+        {'author': {'username': 'Jonathan'},
+         'body': 'Knowledge is your inspiration'}
     ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
 
-    return render_template("index.html", title="Home", user=user, posts=posts)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
